fa33c2bac372adcc823a192979053ce7bc6e2b3c
net <114216794+net-for@users.noreply.github.com>
2025-01-15 04:03:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Function to close the CEF window when the "X" button is clicked
 function closeWindow() {
-    // Use a native event to close the CEF window in the game
-    if (typeof window.external !== "undefined" && window.external.close_browser) {
-        window.external.close_browser();
-    }
+    // Notify the server to close the browser window
+    window.external.emitEvent("close_browser");
 }
